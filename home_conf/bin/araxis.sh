07a05925d2ba9fc0f5e3a02c242b3b72d48aae1a
Gushenghua <gsh123china@163.com>
2025-01-13 15:53:05 +0800

    fix 3way-merge in araxis

--- home_conf/bin/araxis.sh
+++ home_conf/bin/araxis.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-"/mnt/d/Program Files/Araxis/Araxis Merge/Merge.exe" "$(wslpath -w "$1")" "$(wslpath -w "$2")"
+
+# Usage: 
+# 将下列内容加入~/.gitconfig
+#####
+# [diff]
+# 	tool = araxis
+# [difftool "araxis"]
+#	cmd = ~/bin/araxis.sh "$LOCAL" "$REMOTE"
+# [merge]
+#	tool = araxis
+# [mergetool "araxis"]
+#	cmd = ~/bin/araxis.sh "$LOCAL" "$REMOTE" "$BASE" "$MERGED"
+#	trustExitCode = true
+#####
+
+
+# 转换每个参数为 Windows 路径
+args=()
+for arg in "$@"; do
+	args+=("$(wslpath -w "$arg")")
+done
+
+# 调用 Araxis Merge，传入转换后的参数
+"/mnt/d/Program Files/Araxis/Araxis Merge/Merge.exe" "${args[@]}"
 
